7fbcd9e9d2c48b1fb67eef59c50da0bd8e133177
MatAndreoli <andreoli.2003@gmail.com>
2024-12-21 22:39:12 -0300

    remove current_theme file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -75,7 +75,7 @@ require('lazy').setup({
   },
 })
 
-vim.cmd('colorscheme ' .. require 'settings.current_theme')
+vim.cmd 'colorscheme onedark_vivid'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
