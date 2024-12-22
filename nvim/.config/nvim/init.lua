17b98d4ec6f5eb3114955c0feadb817ff8362bec
MatAndreoli <andreoli.2003@gmail.com>
2024-12-22 12:45:50 -0300

    move from telescope to fzf-lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,12 +1,6 @@
 require 'settings.options'
 require 'settings.keymaps'
 
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
@@ -23,8 +17,6 @@ vim.api.nvim_create_autocmd('TermOpen', {
   end,
 })
 
--- [[ Install `lazy.nvim` plugin manager ]]
---    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
@@ -35,28 +27,10 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
---
--- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   { import = 'plugins' },
-
-  -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
-  -- Or use telescope!
-  -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
-  -- you can continue same window with `<space>sr` which resumes last telescope search
 }, {
   ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
     icons = vim.g.have_nerd_font and {} or {
       cmd = '⌘',
       config = '🛠',
---
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,46 @@
+return {
+  'ibhagwan/fzf-lua',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('fzf-lua').setup {
+      winopts = {
+        height = 0.9,
+        width = 0.9,
+        border = 'rounded',
+        preview = {
+          wrap = 'wrap',
+        },
+      },
+      git = {
+        icons = {
+          ['M'] = { icon = '', color = 'yellow' },
+          ['D'] = { icon = '', color = 'red' },
+          ['A'] = { icon = '', color = 'green' },
+          ['R'] = { icon = '󰑕', color = 'yellow' },
+          ['C'] = { icon = '', color = 'yellow' },
+          ['T'] = { icon = '', color = 'magenta' },
+          ['?'] = { icon = '', color = 'magenta' },
+        },
+      },
+    }
+    local builtin = require 'fzf-lua'
+    vim.keymap.set('n', '<leader>sh', builtin.helptags, { desc = '[S]earch [H]elp' })
+    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+    vim.keymap.set('n', '<leader>sf', builtin.files, { desc = '[S]earch [F]iles fzf-lua' })
+    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect fzf-lua' })
+    vim.keymap.set('n', '<leader>sw', builtin.grep_cword, { desc = '[S]earch current [W]ord' })
+    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+    vim.keymap.set('n', '<leader>sd', builtin.diagnostics_document, { desc = '[S]earch [D]iagnostics' })
+    vim.keymap.set('n', '<leader>sD', builtin.diagnostics_workspace, { desc = '[S]earch [D]iagnostics in workspace' })
+    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    vim.keymap.set('n', '<leader>gs', builtin.git_status, { desc = '[G]it [S]tatus' })
+    vim.keymap.set('n', '<leader>gt', builtin.git_stash, { desc = '[G]it s[T]ash' })
+    vim.keymap.set('n', '<leader>gc', builtin.git_commits, { desc = '[G]it [C]ommits' })
+    vim.keymap.set('n', '<leader>gh', builtin.git_bcommits, { desc = '[G]it Commits of current buffer' })
+    vim.keymap.set('n', '<leader>gb', builtin.git_branches, { desc = '[G]it [B]ranches' })
+    vim.keymap.set('n', '<leader>/', builtin.blines, { desc = '[/] Fuzzily search in current buffer' })
+    vim.keymap.set('n', '<leader>s/', builtin.lines, { desc = '[S]earch [/] in Open Files' })
+  end,
+}
