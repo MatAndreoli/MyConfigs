df54710edebe6882a25c90b03f8b647d663865a3
Matheus Andreoli <80846922@telefonicati.onmicrosoft.com>
2025-06-27 09:09:25 -0300

    update lazy-lock, enable grug-gar

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -21,7 +21,6 @@ require('lazy').setup({
 
     -- Disabling plugins
     { 'catppuccin', enabled = false },
-    { 'MagicDuck/grug-far.nvim', enabled = false },
     { 'folke/trouble.nvim', enabled = false },
     { 'nvim-neo-tree/neo-tree.nvim', enabled = false },
     { 'nvim-treesitter/nvim-treesitter-textobjects', enabled = false },
